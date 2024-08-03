7d1211b3ee0bb7b78d07c571c3219196c3e6c76b
Hugo Lapre <dev@tbdwebdesign.nl>
2024-08-03 21:45:22 +0200

    feat: more visible relative line numbers

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,12 @@
 local function ColorMyPencils(color)
-	color = color or "tokyonight"
-	vim.cmd.colorscheme(color)
+  color = color or 'tokyonight'
+  vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+
+  vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#82AAFF', bg = '#222436' })
+  vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#82AAFF', bg = '#222436' })
 end
 
 ColorMyPencils()
