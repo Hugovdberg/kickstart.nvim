b1fa80c6be371672f4e96037b1da37585cc63fc7
Lapre <hugo.lapre@brabantwater.nl>
2026-01-15 13:53:28 +0100

    fix: renamed nvim-treesitter.configs to nvim.treesitter.config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,20 +1,17 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  config = function()
-    local configs = require 'nvim-treesitter.configs'
-
-    configs.setup {
-      ensure_installed = { 'c', 'diff', 'elixir', 'heex', 'html', 'javascript', 'lua', 'markdown', 'markdown_inline', 'python', 'query', 'vim', 'vimdoc' },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-      auto_install = true,
-      ignore_install = {},
-      modules = {},
-      context = { enable = true },
-    }
-  end,
+  main = 'nvim-treesitter.config', -- Sets main module to use for opts
+  opts = {
+    ensure_installed = { 'c', 'diff', 'elixir', 'heex', 'html', 'javascript', 'lua', 'markdown', 'markdown_inline', 'python', 'query', 'vim', 'vimdoc' },
+    sync_install = false,
+    highlight = { enable = true },
+    indent = { enable = true },
+    auto_install = true,
+    ignore_install = {},
+    modules = {},
+    context = { enable = true },
+  },
   dependencies = {
     'nvim-treesitter/nvim-treesitter-context',
   },
